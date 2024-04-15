0cc7b3e6cf6bd42e74b155203f4c3e26cf32a50e
Arun-Joseph <142885106+cyberos01@users.noreply.github.com>
2024-04-16 03:38:46 +0530

    create routes.js

---
+++ app/routes.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+
+    app.get('/',function(req,res){ // İSTEMCİNİN '/' İSTEĞİNE KARŞILIK VEREN KOD BLOĞU
+        res.render('index'); // INDEX VİEW DOSYASI RENDER EDİLDİ
+    });
+
+}
+
+
+
+
